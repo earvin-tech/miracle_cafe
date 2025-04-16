b83d572bc580fb9a965e8912160c665e8a5a3979
earvin-tech <wsl@Earvin>
2025-04-17 02:46:36 +1000

    fix: nav transparency, replaced cafe name with image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,10 +19,12 @@ export default function Header() {
     <>
       <header
         className={`fixed top-0 left-0 w-full z-50 flex items-center justify-between px-6 py-4 transition-colors duration-300 ${
-          isScrolled ? "bg-[#FAEADD] shadow-md" : "bg-[#FAEADD85]"
+          isScrolled ? "bg-[#FAEADD85] shadow-md" : "bg-[#FAEADD]"
         }`}
       >
-        <h1 className="text-xl font-lemon text-black-800">Miracle Cafe</h1>
+        {/* <h1 className="text-xl font-lemon text-black-800">Miracle Cafe</h1> */}
+
+        <img src="/site_images/miracle_name2.png" alt="Miracle Name" className="max-h-8" />
 
         <button
           className="md:hidden text-gray-700"
@@ -43,7 +45,7 @@ export default function Header() {
       {menuOpen && (
         <nav
         className={`fixed top-[72px] left-0 w-full flex flex-col md:hidden items-center font-kopik text-center gap-4 px-6 py-4 shadow-md z-40 transition-colors duration-300 ${
-          isScrolled ? "bg-[#FAEADD85]" : "bg-[#FAEADD85]"
+          isScrolled ? "bg-[#FAEADD]" : "bg-[#FAEADD]"
         }`}
       >
         <Link to="/" className="hover:underline">Home</Link>
