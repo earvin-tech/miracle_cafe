ac51cca6f89ea0acbec019733c053e3f3167b95a
earvin-tech <wsl@Earvin>
2025-04-21 22:14:35 +1000

    Replace hero image

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,20 +1,19 @@
 export default function HeroImage() {
-    return (
-      <div className="relative h-[75vh] sm:h-[85vh] md:h-[90vh]">
+  return (
+    <div className="relative h-[50vh] sm:h-[70vh] md:h-[90vh]">
+      <img 
+        src="/site_images/hero_image_4.jpg" 
+        alt="Coffee Landing Image" 
+        className="w-full h-full object-cover" 
+      />
+
+      <div className="absolute top-[2.5rem] sm:top-[-1.5rem] md:top-[-2rem] left-1/2 -translate-x-1/2">
         <img 
-          src="/site_images/coffee_primary_image[placeholder].jpg" 
-          alt="Coffee Landing Image" 
-          className="w-full h-full object-cover" 
+          src="/site_images/motto_remastered.png" 
+          alt="Motto" 
+          className="w-[200vw] sm:w-[160vw] md:w-[110vw] lg:w-[90vw] max-h-[90vh] object-contain pointer-events-none"
         />
-  
-        <div className="absolute top-[-0.5rem] sm:top-[-1.5rem] md:top-[-2rem] left-1/2 -translate-x-1/2">
-          <img 
-            src="/site_images/motto_remastered.png" 
-            alt="Motto" 
-            className="w-[180vw] sm:w-[140vw] md:w-[110vw] lg:w-[90vw] max-h-[90vh] object-contain pointer-events-none" 
-          />
-        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
